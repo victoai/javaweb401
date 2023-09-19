e2943dd8bbf8b4d540f241425bdeaaad7efd62c1
jooyeon <victoai999@gmail.com>
2023-09-19 13:55:23 +0900

    day19-19

--- testprj/src/main/java/testprj/ReplaceAllTest.java
+++ testprj/src/main/java/testprj/ReplaceAllTest.java
@@ -2,14 +2,11 @@ package testprj;
 
 public class ReplaceAllTest {
 
-	public static void main(String[] args) {
-		 
+	public static void main(String[] args) { 
 		
 		String result="0123";
 		
-		
-		String r= result.replaceAll("[0-3]", "");
-		
+		String r= result.replaceAll("[0-3]", "");		
 		System.out.println( "charAt"+ r.charAt(0));
 		
 		System.out.println( r);
